10bccdf3d364417e45ee05e1028f4dd48950dc1f
Kristijan Vizi <kristijanvizi@Kristijans-MacBook-Pro-2.local>
2024-09-17 09:19:10 +0200

    New: added profile dropdown test cases that cover renew notifications

--- nala/blocks/profile-dropdown/profile-dropdown.test.js
+++ nala/blocks/profile-dropdown/profile-dropdown.test.js
@@ -7,13 +7,14 @@ let profileDropdownPage;
 let signInPage;
 
 const { features } = profiles;
+const anniversaryDateCases = features.slice(1, 3);
 
 test.describe('MAPC sign in flow', () => {
   test.beforeEach(async ({ page, browserName, baseURL, context }) => {
     profileDropdownPage = new ProfileDropdownPage(page);
     signInPage = new SignInPage(page);
     page.on('console', (msg) => {
-      console.log(`${msg.type()}: ${msg.text()}`, msg.type() === 'error' ? msg.location().url : null)
+      console.log(`${msg.type()}: ${msg.text()}`, msg.type() === 'error' ? msg.location().url : null);
     });
     if (!baseURL.includes('partners.stage.adobe.com')) {
       await context.setExtraHTTPHeaders({ authorization: `token ${process.env.HLX_API_KEY}` });
@@ -30,6 +31,7 @@ test.describe('MAPC sign in flow', () => {
   });
 
   test(`${features[0].name},${features[0].tags}`, async ({ page }) => {
+    test.slow();
     const { data, path } = features[0];
     await test.step('Go to the home page', async () => {
       await page.goto(`${path}`);
@@ -52,10 +54,110 @@ test.describe('MAPC sign in flow', () => {
       await expect(profileEmail).toBe(data.profileEmail);
       const primaryContact = await profileDropdownPage.primaryContact;
       await expect(primaryContact).toBeVisible();
+      const profilePartnerLevel = await profileDropdownPage.profilePartnerLevel.textContent();
+      await expect(profilePartnerLevel).toBe(data.profilePartnerLevel);
+    });
+
+    await test.step('Verify edit profile link', async () => {
+      const [editProfileTab] = await Promise.all([
+        page.context().waitForEvent('page'),
+        await profileDropdownPage.editProfileButton.click(),
+      ]);
+      await editProfileTab.waitForLoadState();
+      await expect(editProfileTab.url()).toContain(`${data.editProfileURL}`);
+    });
+
+    await test.step('Verify account management link', async () => {
+      const [accountManagementTab] = await Promise.all([
+        page.context().waitForEvent('page'),
+        await profileDropdownPage.accountManagementButton.click(),
+      ]);
+      await accountManagementTab.waitForLoadState();
+      await expect(accountManagementTab.url()).toContain(`${data.accountManagementURL}`);
+    });
+
+    await test.step('Verify sales center link', async () => {
+      const [salesCenterTab] = await Promise.all([
+        page.context().waitForEvent('page'),
+        await profileDropdownPage.salesCenterButton.click(),
+      ]);
+      await salesCenterTab.waitForLoadState();
+      const urlRegex = new RegExp(`.*${data.salesCenterURL}.*`);
+      await salesCenterTab.waitForURL(urlRegex, { timeout: 20000 });
+      await expect(salesCenterTab.url()).toContain(`${data.salesCenterURL}`);
     });
 
     await test.step('Logout', async () => {
       await profileDropdownPage.logoutButton.click();
+      const pages = await page.context().pages();
+      await expect(pages[0].url())
+        .toContain(data.targetAfterLogout);
+    });
+  });
+
+  anniversaryDateCases.forEach((feature) => {
+    test(`${feature.name},${feature.tags}`, async ({ page }) => {
+      test.slow();
+      await test.step('Go to the home page', async () => {
+        await page.goto(`${feature.path}`);
+        await page.waitForLoadState('domcontentloaded');
+        await profileDropdownPage.signInButton.click();
+      });
+
+      await test.step('Sign in', async () => {
+        await signInPage.signIn(page, `${feature.data.partnerLevel}`);
+      });
+
+      await test.step('Verify profile dropdown after successful login', async () => {
+        await profileDropdownPage.profileIconButton.waitFor({ state: 'visible', timeout: 20000 });
+        await profileDropdownPage.toggleProfileDropdown();
+        const profileImage = await profileDropdownPage.profileImage;
+        await expect(profileImage).toBeVisible();
+        const profileName = await profileDropdownPage.profileName.textContent();
+        await expect(profileName).toBe(feature.data.profileName);
+        const profileEmail = await profileDropdownPage.profileEmail.textContent();
+        await expect(profileEmail).toBe(feature.data.profileEmail);
+        const primaryContact = await profileDropdownPage.primaryContact;
+        await expect(primaryContact).toBeVisible();
+        const profilePartnerLevel = await profileDropdownPage.profilePartnerLevel.textContent();
+        await expect(profilePartnerLevel).toBe(feature.data.profilePartnerLevel);
+        const renewText = profileDropdownPage.getRenewNotification(feature.data.type);
+        await expect(renewText).toContainText(feature.data.profileRenewText);
+      });
+
+      await test.step('Verify edit profile link', async () => {
+        const [editProfileTab] = await Promise.all([
+          page.context().waitForEvent('page'),
+          await profileDropdownPage.editProfileButton.click(),
+        ]);
+        await editProfileTab.waitForLoadState();
+        await expect(editProfileTab.url()).toContain(`${feature.data.editProfileURL}`);
+      });
+
+      await test.step('Verify account management link', async () => {
+        const [accountManagementTab] = await Promise.all([
+          page.context().waitForEvent('page'),
+          await profileDropdownPage.accountManagementButton.click(),
+        ]);
+        await accountManagementTab.waitForLoadState();
+        await expect(accountManagementTab.url()).toContain(`${feature.data.accountManagementURL}`);
+      });
+
+      await test.step('Verify renewal link', async () => {
+        const [renewalTab] = await Promise.all([
+          page.context().waitForEvent('page'),
+          await profileDropdownPage.clickRenewNowButton(feature.data.type),
+        ]);
+        await renewalTab.waitForLoadState();
+        await expect(renewalTab.url()).toContain(`${feature.data.renewNowURL}`);
+      });
+
+      await test.step('Logout', async () => {
+        await profileDropdownPage.logoutButton.click();
+        const pages = await page.context().pages();
+        await expect(pages[0].url())
+          .toContain(feature.data.targetAfterLogout);
+      });
     });
   });
 });
