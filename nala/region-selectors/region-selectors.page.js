494bcc68f8c45393fb5ca8ca86071bba134404fc
Kristijan Vizi <kristijanvizi@Kristijans-MacBook-Pro-2.local>
2024-10-23 13:06:54 +0200

    New: added region selector tests

---
+++ nala/region-selectors/region-selectors.page.js
@@ -0,0 +1,22 @@
+export default class RegionSelectorsPage {
+  constructor(page) {
+    this.page = page;
+    this.regionSelectorPopUp = page.locator('div.region-nav[data-block]');
+    this.profileIconButton = page.locator('.feds-profile-button');
+
+    this.oneTrustBanner = page.getByLabel('Cookie banner');
+    this.oneTrustBannerDoNotEnableButton = page.getByRole('button', { name: 'Don\'t enable' });
+  }
+
+  async clickLinkWithText(text) {
+    await this.page.locator(`div.region-nav a:has-text("${text}")`).click();
+  }
+
+  async getButtonElement(text) {
+    return this.page.getByRole('button', { name: `${text}` });
+  }
+
+  async getPopUpParagraphByText(text) {
+    return this.page.locator(`div[data-valign="middle"] p:has-text("${text}")`);
+  }
+}
