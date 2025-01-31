154ad80273ac49deea5718cb5006e0a88e345063
cod17828 <cod17828@adobe.com>
2025-01-31 11:31:32 +0100

    changes in test

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ const config = {
   outputDir: './test-results',
   globalSetup: './nala/utils/global.setup.js',
   /* Maximum time one test can run for. */
-  timeout: 60 * 1000,
+  timeout: 100 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
