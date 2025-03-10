fa62ccef16c72c84d73d65d7690a01b95c7741e6
Slobodanka <jovicic.slobodanka13@gmail.com>
2025-03-10 16:13:03 +0100

    MWPW-166717: Create nala tests for checking redirections

--- nala/blocks/redirections/redirections.page.js
+++ nala/blocks/redirections/redirections.page.js
@@ -1,40 +1,32 @@
-export default {
-  FeatureName: 'DME redirections flow',
-  features: [
-    {
-      tcid: '1',
-      name: '@verify-partner-search-redirections',
-      path: '/channelpartners/#?martech=off',
-      tags: '@dme-redirections @regression @circleCi',
-      data: {
-        findPartnerLinkText: 'Find a Partner',
-        findPartnerFrenchLinkText: 'Trouver un partenaire',
-        findPartnerGermanLinkText: 'Adobe-Partner suchen',
-        findPartnerItalianLinkText: 'Trova un partner',
-        findPartnerSpanishLinkText: 'Encontrar un partner',
-        findPartnerKorianLinkText: '파트너 찾기',
-        findPartnerChinaLinkText: '查找合作伙伴',
-        findPartnerJapanLinkText: 'パートナーを見つける',
-        enLocalePartnerUrl: 'https://adobe.my.salesforce-sites.com/PartnerSearch?lang=en',
-        frLocalePartnerUrl: 'https://adobe--sfstage1.sandbox.my.salesforce-sites.com/PartnerSearch?lang=fr',
-        deLocalePartnerUrl: 'https://adobe.my.salesforce-sites.com/PartnerSearch?lang=de',
-        itLocalePartnerUrl: 'https://adobe--sfstage1.sandbox.my.salesforce-sites.com/PartnerSearch?lang=it',
-        esLocalePartnerUrl: 'https://adobe--sfstage1.sandbox.my.salesforce-sites.com/PartnerSearch?lang=es',
-        krLocalePartnerUrl: 'https://adobe--sfstage1.sandbox.my.salesforce-sites.com/PartnerSearch?lang=ko',
-        cnLocalePartnerUrl: 'https://adobe--sfstage1.sandbox.my.salesforce-sites.com/PartnerSearch?lang=zh_CN',
-        jpLocalePartnerUrl: 'https://adobe.my.salesforce-sites.com/PartnerSearch?lang=ja',
-        naLocaleSwitchUrl: '/na/channelpartners/#?martech=off',
-        latamLocaleSwitchUrl: '/latam/channelpartners/#?martech=off',
-        emeaLocaleSwitchUrl: '/emea/channelpartners/#?martech=off',
-        frLocaleSwitchUrl: '/fr/channelpartners/#?martech=off',
-        deLocaleSwitchUrl: '/de/channelpartners/#?martech=off',
-        itLocaleSwitchUrl: '/it/channelpartners/#?martech=off',
-        esLocaleSwitchUrl: '/es/channelpartners/#?martech=off',
-        apacLocaleSwitchUrl: '/apac/channelpartners/#?martech=off',
-        krLocaleSwitchUrl: '/kr/channelpartners/#?martech=off',
-        cnLocaleSwitchUrl: '/cn/channelpartners/#?martech=off',
-        jpLocaleSwitchUrl: '/jp/channelpartners/#?martech=off',
-      },
-    },
-  ],
-};
+import { expect } from '@playwright/test';
+
+export default class RedirectionsTest {
+  constructor(page) {
+    this.page = page;
+    this.localeModal = page.locator('#locale-modal-v2');
+    this.localeModalCloseButton = page.locator('#locale-modal-v2 .dialog-close');
+  }
+
+  getFindPartnerByRegion(text) {
+    return this.page.locator(`#feds-nav-wrapper a[href*="/PartnerSearch"]:has-text("${text}")`);
+  }
+
+  async verifyURLRedirection(findPartnerLink, localePartnerURL, localeSwitchURL) {
+    await this.page.goto(localeSwitchURL);
+    await expect(findPartnerLink).toBeVisible();
+
+    if (await this.localeModal.isVisible()) {
+      await this.localeModalCloseButton.click();
+    }
+
+    await findPartnerLink.click();
+
+    const [newTab] = await Promise.all([
+      this.page.waitForEvent('popup'),
+    ]);
+
+    await newTab.waitForLoadState();
+    expect(newTab.url()).toBe(`${localePartnerURL}`);
+    newTab.close();
+  }
+}
