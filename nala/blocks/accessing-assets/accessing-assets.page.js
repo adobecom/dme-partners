bcbd42e97053154c1275e40e8e3a5bb60224db70
sashaCodeTest <kakucasaska@gmail.com>
2025-05-15 10:45:13 +0200

    lint-fix

--- nala/blocks/accessing-assets/accessing-assets.page.js
+++ nala/blocks/accessing-assets/accessing-assets.page.js
@@ -1,7 +1,7 @@
 export default class AccessingAssetsPage {
   constructor(page) {
     this.page = page;
-    this.notFound404 = page.locator('#not-fond');
+    this.notFound404 = page.locator('#not-found');
   }
 
   async notFoundContentCheck() {
