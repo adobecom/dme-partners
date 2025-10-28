5892f5ad1b68788bca8b55678ddb9f7c16bf68ea
cod17828 <cod17828@adobe.com>
2025-10-28 11:59:36 +0100

    update catch to log message instead of throw error

--- edsdme/blocks/feedback/feedback.js
+++ edsdme/blocks/feedback/feedback.js
@@ -200,7 +200,7 @@ async function renderDialog(feedbackButton, formDefinitionUrl, config) {
         userName = `${profileData.firstName} ${profileData.lastName}`;
         userEmail = profileData.email;
       } catch (error) {
-        throw new Error(`Failed to parse CPP cookie:: ${error}`);
+        console.info('Failed to parse profileData from cookie:', error);
       }
     }
     const payload = {
