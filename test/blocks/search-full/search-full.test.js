add1763c98f9248bf7a6b40607a6d76d2bd80879
Dejan Bozic <2dmsoftware@gmail.com>
2026-02-10 09:45:26 +0100

    Update test/blocks/search-full/search-full.test.js
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/blocks/search-full/search-full.test.js
+++ test/blocks/search-full/search-full.test.js
@@ -247,7 +247,6 @@ describe('SearchCard Unit Tests', () => {
       searchComponent.isTypeaheadOpen = false;
       const getSuggestionsStub = sinon.stub(searchComponent, 'getSuggestions').resolves(['suggestion1', 'suggestion2']);
       await searchComponent.updateTypeaheadDialog();
-      
       expect(searchComponent.isTypeaheadOpen).to.be.true;
     });
 
