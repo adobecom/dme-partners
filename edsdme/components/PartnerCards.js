434eb4185420c3f14b6e5e0e061ece3c1c213295
draganatrajkovic <dragana.trajkovic@hitthecode.com>
2026-03-24 14:24:34 +0100

    MWPW-190875 - filters with '-&-'

--- edsdme/components/PartnerCards.js
+++ edsdme/components/PartnerCards.js
@@ -109,6 +109,7 @@ export default class PartnerCards extends LitElement {
             parentKey: filterKey,
             value: blockData.localizedText[`{{${tagKey}}}`],
             checked: false,
+            paramValue: tagKey.includes('-&-') ? tagKey.replace('-&-', '-and-') : null,
             initialHidden,
           };
         }
@@ -367,7 +368,8 @@ export default class PartnerCards extends LitElement {
           const filtersSearchTags = this.urlSearchParams.get(filter.key).split(',');
 
           filtersSearchTags.forEach((searchTag) => {
-            const filterTag = filter.tags.find((tag) => tag.key === searchTag);
+            // eslint-disable-next-line max-len
+            const filterTag = filter.tags.find((tag) => tag.paramValue === searchTag || tag.key === searchTag);
             if (filterTag) {
               filterTag.checked = true;
               this.selectedFilters = {
@@ -785,7 +787,7 @@ export default class PartnerCards extends LitElement {
       };
 
       let filterSearchValue = this.urlSearchParams.get(filterKey);
-      filterSearchValue += `,${tag.key}`;
+      filterSearchValue += tag.paramValue ? `,${tag.paramValue}` : `,${tag.key}`;
       this.urlSearchParams.set(filterKey, filterSearchValue);
     } else {
       if (!Object.keys(this.selectedFilters).length) {
@@ -797,7 +799,8 @@ export default class PartnerCards extends LitElement {
         [filterKey]: [tag],
       };
 
-      this.urlSearchParams.append(filterKey, tag.key);
+      const tagParamValue = tag.paramValue ? tag.paramValue : tag.key;
+      this.urlSearchParams.append(filterKey, tagParamValue);
     }
 
     this.paginationCounter = 1;
@@ -807,7 +810,7 @@ export default class PartnerCards extends LitElement {
 
   handleRemoveTag(tag) {
     tag.checked = false;
-    const { key: tagKey, parentKey: filterKey } = tag;
+    const { key: tagKey, parentKey: filterKey, paramValue } = tag;
 
     // eslint-disable-next-line max-len
     const updatedFilterTags = [...this.selectedFilters[filterKey]].filter((filterTag) => filterTag.key !== tagKey);
@@ -819,7 +822,8 @@ export default class PartnerCards extends LitElement {
       };
 
       const filterSearchParams = this.urlSearchParams.get(filterKey).split(',');
-      const updatedSearchFilterTags = filterSearchParams.filter((param) => param !== tagKey);
+      // eslint-disable-next-line max-len
+      const updatedSearchFilterTags = filterSearchParams.filter((param) => param !== paramValue && param !== tagKey);
       this.urlSearchParams.set(filterKey, updatedSearchFilterTags.toString());
     } else {
       const { [filterKey]: _removedKeyFilters, ...updatedSelectedFilters } = this.selectedFilters;
