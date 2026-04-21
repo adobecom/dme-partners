9712bfc5bfcaeaddbc1d81446a220530c6606e4e
Slobodanka <jovicic.slobodanka13@gmail.com>
2026-04-21 11:39:16 +0200

    Update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,11 +22,29 @@ module.exports = {
     'no-unused-expressions': 0,
     'chai-friendly/no-unused-expressions': 2,
     'import/prefer-default-export': 'off',
+    'import/no-cycle': 0,
+    'no-await-in-loop': 0,
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
   },
   overrides: [
     {
       files: ['test/**/*.js', 'nala/**'],
       rules: { 
+      rules: {
         'no-console': 'off',
         'max-len': 'off',
         'func-names': 'off',
