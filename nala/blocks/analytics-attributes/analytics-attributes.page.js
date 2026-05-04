6fde34d4862c9bd5b85e556d8485a01ab07eed28
sashaCodeTest <kakucasaska@gmail.com>
2026-05-04 16:27:16 +0200

    mapc- analytics-attributes-test

---
+++ nala/blocks/analytics-attributes/analytics-attributes.page.js
@@ -0,0 +1,27 @@
+export default class AnalyticsAttributesPage {
+  constructor(page) {
+    this.page = page;
+    this.searchField = page.getByRole('searchbox', { name: 'Search' });
+    this.feedbackButton = page.locator('.feedback-mechanism');
+  }
+
+  byDaaLh(daaLh) {
+    return this.page.locator(`[daa-lh="${daaLh}"]`);
+  }
+
+  async search(searchKeyWord) {
+    const { searchField } = this;
+    await searchField.waitFor({ state: 'visible', timeout: 30000 });
+    await searchField.fill(searchKeyWord);
+    await searchField.press('Enter');
+  }
+
+  getFilter(filter) {
+    const filterElement = this.page.getByLabel(`${filter}`);
+    filterElement.click();
+  }
+
+  getCheckBox(checkBoxName) {
+    return this.page.getByRole('checkbox', { name: `${checkBoxName}` });
+  }
+}
