455ff30a15628f4bb9f91c358434b5b92073e1e4
Slobodanka <jovicic.slobodanka13@gmail.com>
2026-07-28 11:34:31 +0200

    Small code fix

--- edsdme/scripts/utils.js
+++ edsdme/scripts/utils.js
@@ -188,8 +188,7 @@ export function getMetadata(name) {
 }
 
 export function redirectLoggedinPartner(win = window) {
-  if (!isMember()) return;
-  if (getMetadataContent('adobe-loggedin-no-redirect')) return;
+  if (!isMember() || getMetadataContent('adobe-loggedin-no-redirect')) return;
   const partnerErrorRedirectsCount = getCookieValue(PARTNER_ERROR_REDIRECTS_COUNT_COOKIE);
   if (partnerErrorRedirectsCount) {
     const count = Number(partnerErrorRedirectsCount);
