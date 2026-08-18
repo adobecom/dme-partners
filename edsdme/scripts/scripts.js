fb935fba80282fabed6034692999db17149338c0
cod17828 <cod17828@adobe.com>
2026-08-18 11:41:56 +0200

    dummy-log

--- edsdme/scripts/scripts.js
+++ edsdme/scripts/scripts.js
@@ -134,6 +134,7 @@ function setUpPage() {
   await loadArea();
   applyPagePersonalization();
   rewriteLinks(document);
+  console.log('if window.location.host add sidekickListener');
   if (previewHosts.includes(window.location.host)) {
     sidekickListener(CONFIG.locales);
   }
